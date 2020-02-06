7835ed76bd46502d4623280aaa77bb216f241967
arekksulol <alex_palmeraparra2234@hotmial.com>
2020-02-06 12:26:14 -0700

    niko niko niii

---
+++ programa1/programa1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa1
+{
+    class Program
+    {
+        private static object console;
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("introduce un numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("introduce un numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+            Console.WriteLine("La suma es de los numeros es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
